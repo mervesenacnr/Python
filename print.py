48c95889b66c8bd0bfc0f7ad08f4c537622c4b23
Merve Sena Çınar <108677888+mervesenacnr@users.noreply.github.com>
2024-01-10 04:22:11 +0300

    Add files via upload

---
+++ print.py
@@ -0,0 +1,41 @@
+#print() yapısı ve tırnak kullanımı
+print("Python Programlama Dili Çalışmalarım")
+print('Python Programlama Dili Çalışmalarım')
+print("""Python Programlama Dili Çalışmalarım""")
+
+#print yapısı ve üç tırnak kullanımı
+print("""İstanbul'un en sevdiğim yanıdır boğazı.""")
+print("""Ne demiş ulu önder Atatürk: "Yerinde duran, geriye gidiyor demektir… 
+İleri, daima ileri! Düşünmeyen beyinler, düşüncesizlere esir olmaktan öteye gidemezler." """)
+
+"""
+Üç tırnak işaretlerini her türlü karakter dizisiyle birlikte kullanabiliyor olsak da,
+bu tırnak tipi çoğunlukla sadece birden fazla satıra yayılmış karakter dizilerini tanımlamada kullanılır.
+"""
+print("""
+[H]=========HARMAN========[-][o][x]
+|                                 |
+|     Programa Hoşgeldiniz!       |
+|           Sürüm 0.8             |
+|    Devam etmek için herhangi    |
+|       bir düğmeye basın.        |
+|                                 |
+|=================================|
+""")
+
+#type() yapısının kullanımı
+print(type("hi"))
+print(type(12))
+print(type(1.4))
+
+#objeler+print kullanımı
+dil = "Python"
+print("Programlama dili: "+ dil)
+
+
+
+
+
+
+
+
